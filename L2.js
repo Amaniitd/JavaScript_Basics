e693bf407f93b6832bd810cc5a0356993694d34b
Amaniitd <amanamanwrs121@gmail.com>
2022-05-08 09:50:23 +0530

    L2 done

--- L2.js
+++ L2.js
@@ -80,3 +80,66 @@ function f4(arg1, arg2, arg3) {
 //        DOM                Call Stack  | Task/Event Queue         setTimeout(cb, ms)
 //                                                                  xhr
 //                                                                  onclick(cb)
+
+// For things like setTimeout, onclick.. -> we need to store the arguments
+
+
+// Safegaurding the context
+
+function Product(name, brand) {
+    this.name = name;
+    this.brand = brand;
+
+    this.getInfo = function () {
+        setTimeout(
+        function getInfoFromServer() {
+            this.data = [10, 20, 30]; // here this will not p1 | window.data = [10, 20, 30];
+        }, 1000);
+    }
+}
+// This will not output error but
+// the problem is "this" pointer will changing the location
+
+// How to fix this?
+// Use "self" keyword
+
+function Product(name, brand) {
+    this.name = name;
+    this.brand = brand;
+    this.getInfo = function () {
+        var self = this; // we will store the location of the object in the variable "self"
+        setTimeout(
+            function getInfoFromServer() {
+                self.data = [10, 20, 30]; // here this will be p1
+            }, 1000);
+    }
+}
+
+// This is called "safegaurding the context"
+
+var p1 = new Product("Iphone", "Apple");
+p1.getInfo();
+// wait for 1000 msec before calling console log
+console.log(p1.data); // undefined
+setTimeout(function () {
+    console.log(p1.data);
+}, 1000);
+
+
+// Second way:- Safegaurding the context using bind
+// We will educate the function that it use the correct context
+// We will use "bind" method
+
+function Product(name, brand) {
+    this.name = name;
+    this.brand = brand;
+    function getInfoFromServer() {
+        this.data = [10, 20, 30];
+    }
+    this.getInfo = function () {
+        setTimeout(getInfoFromServer.bind(this), 1000);
+    }
+}
+
+// Not only "this" we can bind other and multiple arguments/variables
+
